902dfb2b88aa57281e2a2ac93bfb26f667a70649
sleepycreamcake <younglaiengchi@gmail.com>
2025-03-06 17:06:38 -0800

    connect to supabase, set up environment

---
+++ pages/api/users.js
@@ -0,0 +1,9 @@
+import supabase from "../../lib/supabase";
+
+export default async function handler(req, res) {
+  const { data, error } = await supabase.from("users").select("*");
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+  res.status(200).json(data);
+}
